e81efa73521079eebc80ad20ab615e117121f0b7
Michele Prova <michele.prova99@gmail.com>
2020-07-22 16:46:37 +0200

    fine

---
+++ main.js
@@ -0,0 +1,14 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+
+$(".header-right i").click(function(){
+
+  $(".hamburger-menu").show("slow");
+
+});
+
+$(".close i").click(function(){
+
+  $(".hamburger-menu").hide("slow");
+
+});
