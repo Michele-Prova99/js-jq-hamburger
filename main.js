3d1bd57608a01888d510727bdba6a9bf50728b0b
Michele Prova <michele.prova99@gmail.com>
2020-07-23 17:14:30 +0200

    corretto

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Hamburger menu:
 // mostrare / nascondere il menu principale
 
-$(".header-right i").click(function(){
+$(".header-right > a").click(function(){
 
   $(".hamburger-menu").show("slow");
 
 });
 
-$(".close i").click(function(){
+$(".close").click(function(){
 
   $(".hamburger-menu").hide("slow");
 
